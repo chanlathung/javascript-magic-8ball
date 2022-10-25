1c79614f096f7df1b447a5a302c12b3b8925d3a7
chanlathung <63987068+chanlathung@users.noreply.github.com>
2022-10-25 15:53:05 -0700

    Ask a question page complete.
    - Has text area and submit button.
    - Has "hidden" property.
    - JavaScript eventListener works when PLAY button on landing page is clicked.
    - Styling complete.

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,7 @@
 console.log("Hello, World!");
+
+document.getElementById("play")
+        .addEventListener("click", () => {
+  document.getElementById("start").hidden = true;
+  document.getElementById("ask").hidden = false;
+}, false);
