79bcf83b98ea552a7987574a20f071d13d8fee12
chanlathung <63987068+chanlathung@users.noreply.github.com>
2022-10-25 18:01:19 -0700

    created div
    -finish javascript answers
    -needs styling

--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,9 @@ document.getElementById("play")
   document.getElementById("start").hidden = true;
   document.getElementById("ask").hidden = false;
 }, false);
+
+document.getElementById("submit-question")
+        .addEventListener("click", () => {
+  document.getElementById("ask").hidden = true;
+  document.getElementById("ellipse").hidden = false;
+}, false);
