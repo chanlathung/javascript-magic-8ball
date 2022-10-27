051feea4767c671014c69241b3cfd2c23f4bafaf
chanlathung <63987068+chanlathung@users.noreply.github.com>
2022-10-27 01:02:46 -0700

    Random image styled

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,15 @@ document.getElementById("play")
   document.getElementById("ask").hidden = false;
 }, false);
 
+document.getElementById("submit_question")
+        .addEventListener("click", () => {
+  document.getElementById("ask").hidden = true;
+  document.getElementById("eight_ball").hidden = false;
+}, false);
+
 const arrayOfImages = ["IMG/image1.jpg", "IMG/image2.jpg", "IMG/image3.jpg", "IMG/image4.jpg", "IMG/image5.jpg", "IMG/image6.jpg", "IMG/image7.jpg", "IMG/image8.jpg"];
+
+let selectedImage = "";
 
 function getRandomImage() {
   // Get a random index
@@ -21,3 +29,4 @@ function getRandomImage() {
   document.getElementById("image").src = `./${selectedImage}`;
 
 }
+
