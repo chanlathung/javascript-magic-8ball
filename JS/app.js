3d2419314de9e4bf38a50d90cf757d77d137d087
chanlathung <63987068+chanlathung@users.noreply.github.com>
2022-10-26 23:54:21 -0700

    Generates random image and displays in in HTML

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,6 @@
+
 console.log("Hello, World!");
+
 
 document.getElementById("play")
         .addEventListener("click", () => {
@@ -6,8 +8,16 @@ document.getElementById("play")
   document.getElementById("ask").hidden = false;
 }, false);
 
-document.getElementById("submit-question")
-        .addEventListener("click", () => {
-  document.getElementById("ask").hidden = true;
-  document.getElementById("ellipse").hidden = false;
-}, false);
+const arrayOfImages = ["IMG/image1.jpg", "IMG/image2.jpg", "IMG/image3.jpg", "IMG/image4.jpg", "IMG/image5.jpg", "IMG/image6.jpg", "IMG/image7.jpg", "IMG/image8.jpg"];
+
+function getRandomImage() {
+  // Get a random index
+  const randomIndex = Math.floor(Math.random() * arrayOfImages.length);
+
+  // Get an image at the index
+  const selectedImage = arrayOfImages[randomIndex];
+
+  // Display the image
+  document.getElementById("image").src = `./${selectedImage}`;
+
+}
