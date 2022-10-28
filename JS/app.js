149f84d4ef49ff44985deecf086062a716df1a12
chanlathung <63987068+chanlathung@users.noreply.github.com>
2022-10-27 18:17:14 -0700

    Removed dead code.

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,16 @@
+const arrayOfImages = ["IMG/image1.jpg", "IMG/image2.jpg", "IMG/image3.jpg", "IMG/image4.jpg", "IMG/image5.jpg", "IMG/image6.jpg", "IMG/image7.jpg", "IMG/image8.jpg"];
 
-console.log("Hello, World!");
+function getRandomImage() {
+  // Get a random index
+  const randomIndex = Math.floor(Math.random() * arrayOfImages.length);
 
+  // Get an image at the index
+  const selectedImage = arrayOfImages[randomIndex];
+
+  // Display the image
+  document.getElementById("image").src = `./${selectedImage}`;
+
+}
 
 document.getElementById("play")
         .addEventListener("click", () => {
@@ -15,18 +25,4 @@ document.getElementById("submit_question")
   document.getElementById("text").hidden = false;
   document.getElementById("image").hidden = false;
 }, false);
-
-const arrayOfImages = ["IMG/image1.jpg", "IMG/image2.jpg", "IMG/image3.jpg", "IMG/image4.jpg", "IMG/image5.jpg", "IMG/image6.jpg", "IMG/image7.jpg", "IMG/image8.jpg"];
-
-function getRandomImage() {
-  // Get a random index
-  const randomIndex = Math.floor(Math.random() * arrayOfImages.length);
-
-  // Get an image at the index
-  const selectedImage = arrayOfImages[randomIndex];
-
-  // Display the image
-  document.getElementById("image").src = `./${selectedImage}`;
-
-}
 
